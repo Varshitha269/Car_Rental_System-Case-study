b2e86e02b563bde34074012560d58e0341dfa358
Govindaswamy Varshitha <99487978+Varshitha269@users.noreply.github.com>
2024-05-21 14:02:31 +0530

    Add files via upload

---
+++ Car Rental System/Program.cs
@@ -0,0 +1,15 @@
+﻿using Car_Rental_System.Main;
+
+namespace Car_Rental_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            CarRentalSystemApplication crsa = new CarRentalSystemApplication();
+            crsa.Run();
+        }
+    }
+}
---
+++ Car Rental System/Util/PropertyUtil.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental_System.Util
+{
+    //internal class PropertyUtil
+    //{
+    //}
+
+    public static class PropertyUtil
+    {
+        private static IConfiguration _iconfiguration;
+
+
+        public static string GetConnectionString()
+        {
+            return _iconfiguration.GetConnectionString("LocalConnectionString");
+        }
+
+
+        private static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+            _iconfiguration = builder.Build();
+
+        }
+
+        static PropertyUtil()
+
+        {
+            GetAppSettingsFile();
+        }
+
+    }
+
+}
